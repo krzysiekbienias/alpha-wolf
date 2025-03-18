2aa8359eb276551449eb54f88fe6e5cebe0a368d
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-03-18 14:06:20 +0100

    main: Update main file.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-
 import os
 import django
 import pandas as pd
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 django.setup()
-from efficient_frontier.services.yahoo_finance_client import YahooDataExtractor,import_data_from_csv
-
+from efficient_frontier.services.market_data_client import DataProviderFactory, MarketDataExtractor
+from typing import List
+from tool_kit.database_api import fetch_and_store_company_info, fetch_and_store_equity_prices
 
 if __name__ == '__main__':
-    yahoo_extractor: dict = YahooDataExtractor(tickers=['TSLA','AAPL'], start_period='2023-01-24',
-                                               end_period='2025-01-24').extract_data()
+    # ===========================================
+    # REGION: Input
+    # ===========================================
+    provider = 'PolygonIO'
+    company_tickers: List[str] | str | None = None
+    start_time_window: str | None = '2025-03-07'
+    end_time_window: str | None = '2025-03-17'
+
+    # ===========================================
+    # END REGION: Input
+    # ===========================================
+    extractor = DataProviderFactory.get_extractor(provider)
+    extractor.set_start_period(start_time_window)
+    extractor.get_fx_close_price(fx_pair="C:EURGBP",
+                                 date=start_time_window)
+
     print("Hello World")
